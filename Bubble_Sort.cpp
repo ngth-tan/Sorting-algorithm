3060a18bbca5263707865ed1983a9ef957303efc
Nguyen Tan Thanh <tn873793@gmail.com>
2026-06-05 20:26:55 +0700

    Add Bubble Sort and Insertion Sort

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int BubbleSort(int a[], int n) {
+   for(int i = 0; i < n - 1; i++) {
+      for(int j = n - 1; j > i - 1; j--) {
+         if(a[j] < a[j - 1]) {
+            swap(a[j], a[j - 1]);
+         }
+      }
+   }
+   return a[n];
+}
